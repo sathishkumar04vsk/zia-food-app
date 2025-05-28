250ce020f0be15e34372977d926501b2a645835c
sathishkumar04vsk <sathish04vsk@gmail.com>
2025-05-28 20:31:25 +0530

    useEffect

--- src/components/FoodList.jsx
+++ src/components/FoodList.jsx
@@ -1,8 +1,32 @@
 import { Card, CardContent, CardMedia } from "@mui/material";
-import React from "react";
 import StarsIcon from "@mui/icons-material/Stars";
+import { useEffect, useState } from "react";
 
 export default function FoodList(){
+    const [count, setCount] = useState(1);
+    const [name, setName] = useState('');
+    // useEffect 
+
+    // useEffect(start,dependency)
+    // useEffect (()=>{}, [count])
+    // console.log(count)
+    // console.log("hello world");
+
+    
+
+    useEffect(()=>{
+        console.log(count);
+        console.log("Hello World")
+        setCount(count+1)
+    },[]);//one time callback or initil dom rendering
+    
+
+
+    useEffect(()=>{
+        console.log(count);
+        console.log("HELLO WORLD")
+    }, [name]);
+
     return <div className="container mt-24 mb-12 mx-auto">
             <div className="grid grid-cols-4">
                 <Card>
@@ -22,6 +46,16 @@ export default function FoodList(){
                             </div>
                         </CardContent>
                 </Card>
+                <button onClick={()=> setCount(count+1)}>count</button>
+                <input value={name} onChange={event=>setName(event.target.value)} type="text" />
+                {count}
             </div>
     </div>
 };
+
+
+// 1.alt => making more then one cursor
+// 2.crtl + d => find same string
+// 3. crtl + f => search
+// 4. crtl + shit + p => emmit wrap <div> </div> 
+
