0e861bdc9a1562361674552fcd5e12f8705d2ffe
sathishkumar04vsk <sathish04vsk@gmail.com>
2025-06-17 08:53:33 +0530

    form validation added

--- src/components/AddFood.jsx
+++ src/components/AddFood.jsx
@@ -2,54 +2,173 @@ import { Button, TextField } from "@mui/material";
 import React, { useContext, useState } from "react";
 import { APPContext } from "../App";
 import { useNavigate } from "react-router-dom";
+import * as Yup from "yup";
+import { useFormik } from "formik";
 
-export function AddFood(){
-    const navigate = useNavigate()
-    const [formdata, setFormData] = useState({
-            restorant_name:"",
-            food_image:"",
-            rating:0,
-            expected_delivery: "",
-            food_name:"",
-            location:""
+export const ValidationSchema = Yup.object({
+  restorant_name: Yup.string().required("Restorant Name is required"),
+  food_image: Yup.string().url().required("Food Image is required"),
+  rating: Yup.number().min(1).max(5).required("Rating is required"),
+  expected_delivery: Yup.string().required(
+    "Expected Delivery Time is required"
+  ),
+  food_name: Yup.string().required("Food Name is required"),
+  location: Yup.string().required("Location is required"),
+});
+
+export function AddFood() {
+  const navigate = useNavigate();
+ 
+
+  const formik = useFormik({
+    initialValues: {
+      restorant_name: "",
+      food_image: "",
+      rating: 1,
+      expected_delivery: "",
+      food_name: "",
+      location: "",
+    },
+    validationSchema: ValidationSchema,
+    onSubmit: (values) => {
+      createFood(values);
+    },
+  });
+
+ 
+
+  const createFood = (values) => {
+    try {
+      const payload = JSON.stringify(values);
+      fetch("https://61d2867cda87830017e59561.mockapi.io/foods", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: payload,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          navigate("/");
         })
+        .catch((error) => console.log(error));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-        console.log(formdata)
-    
-        const handleOnChage  = (event)=>{
-            console.log(event);
-            const key = event.target.name
-            setFormData(prev=>({...prev,[key]:event.target.value}))
-            //                 yped va     restorant_name:tlue  
-            //                      food_image: typed value  
-        }
-
-        const createFood = () =>{
-            try {
-                const payload = JSON.stringify(formdata)
-                fetch("https://61d2867cda87830017e59561.mockapi.io/foods",{method:"POST", headers:{'content-type':'application/json'},body:payload}).then((response)=>response.json()).then((data)=>{console.log(data); navigate('/');}).catch(error=>console.log(error))
-            } catch (error) {
-                console.log(error)
-            }
-        }
-
-        const handleSubmit = ()=>{
-            createFood();
-            
-        }
-
-        
-
-    return<div className="my-4 max-w-[960px] mx-auto">
-        <h1 className="text-2xl mb-8 font-bold text-center">Add Your Restorant Food</h1>
-        <form className="grid gap-4" >
-            <TextField fullWidth onChange={(event)=>setFormData(prev=>({...prev,restorant_name: event.target.value}))} value={formdata.restorant_name} className="!mb-2" id="restorant_name" name="restorant_name" label="Restorant Name" />
-            <TextField fullWidth onChange={handleOnChage} value={formdata.food_image} className="!mb-2" type="url" id="food_image" name="food_image" label="Food Image URL" />
-            <TextField fullWidth onChange={handleOnChage} value={formdata.rating} className="!mb-2" type="number" id="rating" name="rating" label="Rating" />
-            <TextField fullWidth onChange={handleOnChage} value={formdata.food_name} className="!mb-2" id="food_name" name="food_name" label="Food Name" />
-            <TextField fullWidth onChange={handleOnChage} value={formdata.expected_delivery} className="!mb-2" id="expected_delivery" name="expected_delivery" label="Expected Delivery Time (mins)" />
-            <TextField fullWidth onChange={handleOnChage} value={formdata.location} className="!mb-2" id="location" name="location" label="Location" />
-            <div className="flex justify-end"><Button onClick={handleSubmit} variant="contained">Save</Button></div>
-        </form>
+ 
+  return (
+    <div className="my-4 max-w-[960px] mx-auto">
+      <h1 className="text-2xl mb-8 font-bold text-center">
+        Add Your Restorant Food
+      </h1>
+      <form onSubmit={formik.handleSubmit} className="grid gap-4">
+        <TextField
+          fullWidth
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          error={formik.errors.restorant_name && formik.touched.restorant_name}
+          helperText={
+            formik.errors.restorant_name &&
+            formik.touched.restorant_name &&
+            formik.errors.restorant_name
+          }
+          value={formik.values.restorant_name}
+          className="!mb-2"
+          id="restorant_name"
+          name="restorant_name"
+          label="Restorant Name"
+        />
+        <TextField
+          fullWidth
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          error={formik.errors.food_image && formik.touched.food_image}
+          helperText={
+            formik.errors.food_image &&
+            formik.touched.food_image &&
+            formik.errors.food_image
+          }
+          value={formik.values.food_image}
+          className="!mb-2"
+          type="url"
+          id="food_image"
+          name="food_image"
+          label="Food Image URL"
+        />
+        <TextField
+          fullWidth
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          error={formik.errors.rating && formik.touched.rating}
+          helperText={
+            formik.errors.rating &&
+            formik.touched.rating &&
+            formik.errors.rating
+          }
+          value={formik.values.rating}
+          className="!mb-2"
+          type="number"
+          id="rating"
+          name="rating"
+          label="Rating"
+        />
+        <TextField
+          fullWidth
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          error={formik.errors.food_name && formik.touched.food_name}
+          helperText={
+            formik.errors.food_name &&
+            formik.touched.food_name &&
+            formik.errors.food_name
+          }
+          value={formik.values.food_name}
+          className="!mb-2"
+          id="food_name"
+          name="food_name"
+          label="Food Name"
+        />
+        <TextField
+          fullWidth
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          error={
+            formik.errors.expected_delivery && formik.touched.expected_delivery
+          }
+          helperText={
+            formik.errors.expected_delivery &&
+            formik.touched.expected_delivery &&
+            formik.errors.expected_delivery
+          }
+          value={formik.values.expected_delivery}
+          className="!mb-2"
+          id="expected_delivery"
+          name="expected_delivery"
+          label="Expected Delivery Time (mins)"
+        />
+        <TextField
+          fullWidth
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          error={formik.errors.location && formik.touched.location}
+          helperText={
+            formik.errors.location &&
+            formik.touched.location &&
+            formik.errors.location
+          }
+          value={formik.values.location}
+          className="!mb-2"
+          id="location"
+          name="location"
+          label="Location"
+        />
+        <div className="flex justify-end">
+          <Button type="submit" variant="contained">
+            Save
+          </Button>
+        </div>
+      </form>
     </div>
+  );
 }
--- src/components/UpdateFood.jsx
+++ src/components/UpdateFood.jsx
@@ -2,20 +2,13 @@ import React, { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { APPContext } from "../App";
 import { Button, TextField } from "@mui/material";
+import { useFormik } from "formik";
+import { ValidationSchema } from "./AddFood";
 
 export default function UpdateFood() {
-  const navigate = useNavigate();
   const { id } = useParams();
-
   const [formdata, setFormData] = useState(null);
-  const handleOnChage = (event) => {
-    console.log(event);
-    const key = event.target.name;
-    setFormData((prev) => ({ ...prev, [key]: event.target.value }));
-    //                 yped va     restorant_name:tlue
-    //                      food_image: typed value
-  };
-
+ 
   const fetchFood = () => {
     try {
       fetch(`https://61d2867cda87830017e59561.mockapi.io/foods/${id}`, {
@@ -27,9 +20,40 @@ export default function UpdateFood() {
     } catch (error) {}
   };
 
-  const updateFood = () => {
+ 
+  useEffect(() => {
+    fetchFood();
+  }, []);
+
+  
+  return (
+    <div className="my-4 max-w-[960px] mx-auto">
+      <h1 className="text-2xl font-bold mb-8 text-center">
+        Edit Your Restorant Food
+      </h1>
+      {formdata ? (
+        <FromComponent initialValues={formdata} />
+      ) : (
+        <p>Loading....</p>
+      )}
+    </div>
+  );
+}
+
+
+const FromComponent  = ({initialValues}) =>{
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const formik = useFormik({
+    initialValues: initialValues,
+    validationSchema: ValidationSchema,
+    onSubmit: (values) => {
+      updateFood(values);
+    },
+  });
+   const updateFood = (values) => {
     try {
-      const paylod = JSON.stringify(formdata);
+      const paylod = JSON.stringify(values);
       fetch(`https://61d2867cda87830017e59561.mockapi.io/foods/${id}`, {
         method: "PUT",
         headers: { "content-type": "application/json" },
@@ -46,30 +70,22 @@ export default function UpdateFood() {
     }
   };
 
-  const handleSubmit = () => {
-    updateFood();
-  };
-
-  useEffect(() => {
-    fetchFood();
-  }, []);
-
-  return (
-    <div className="my-4 max-w-[960px] mx-auto">
-      <h1 className="text-2xl font-bold mb-8 text-center">
-        Edit Your Restorant Food
-      </h1>
-      {formdata ? (
-        <form className="grid gap-4">
+  const { handleSubmit, handleChange, handleBlur, touched, errors, values } =
+    formik;
+  return <form onSubmit={handleSubmit} className="grid gap-4">
           <TextField
             fullWidth
-            onChange={(event) =>
-              setFormData((prev) => ({
-                ...prev,
-                restorant_name: event.target.value,
-              }))
+            onBlur={handleBlur}
+            onChange={handleChange}
+            value={values.restorant_name}
+            error={
+             errors.restorant_name &&touched.restorant_name
             }
-            value={formdata.restorant_name}
+            helperText={
+             errors.restorant_name &&
+             touched.restorant_name &&
+             errors.restorant_name
+            }
             className="!mb-2"
             id="restorant_name"
             name="restorant_name"
@@ -77,8 +93,15 @@ export default function UpdateFood() {
           />
           <TextField
             fullWidth
-            onChange={handleOnChage}
-            value={formdata.food_image}
+            onBlur={handleBlur}
+            onChange={handleChange}
+            value={values.food_image}
+            error={errors.food_image &&touched.food_image}
+            helperText={
+             errors.food_image &&
+             touched.food_image &&
+             errors.food_image
+            }
             className="!mb-2"
             type="url"
             id="food_image"
@@ -87,8 +110,15 @@ export default function UpdateFood() {
           />
           <TextField
             fullWidth
-            onChange={handleOnChage}
-            value={formdata.rating}
+            onBlur={handleBlur}
+            onChange={handleChange}
+            value={values.rating}
+            error={errors.rating && touched.rating}
+            helperText={
+             errors.rating &&
+             touched.rating &&
+             errors.rating
+            }
             className="!mb-2"
             type="number"
             id="rating"
@@ -97,8 +127,15 @@ export default function UpdateFood() {
           />
           <TextField
             fullWidth
-            onChange={handleOnChage}
-            value={formdata.food_name}
+            onBlur={handleBlur}
+            onChange={handleChange}
+            value={values.food_name}
+            error={errors.food_name &&touched.food_name}
+            helperText={
+             errors.food_name &&
+             touched.food_name &&
+             errors.food_name
+            }
             className="!mb-2"
             id="food_name"
             name="food_name"
@@ -106,8 +143,18 @@ export default function UpdateFood() {
           />
           <TextField
             fullWidth
-            onChange={handleOnChage}
-            value={formdata.expected_delivery}
+            onBlur={handleBlur}
+            onChange={handleChange}
+            value={values.expected_delivery}
+            error={
+             errors.expected_delivery &&
+             touched.expected_delivery
+            }
+            helperText={
+             errors.expected_delivery &&
+              touched.expected_delivery &&
+              errors.expected_delivery
+            }
             className="!mb-2"
             id="expected_delivery"
             name="expected_delivery"
@@ -115,22 +162,24 @@ export default function UpdateFood() {
           />
           <TextField
             fullWidth
-            onChange={handleOnChage}
-            value={formdata.location}
+            onBlur={handleBlur}
+            onChange={handleChange}
+            value={values.location}
+            error={errors.location && touched.location}
+            helperText={
+              errors.location &&
+              touched.location &&
+              errors.location
+            }
             className="!mb-2"
             id="location"
             name="location"
             label="Location"
           />
           <div className="flex justify-end">
-            <Button onClick={handleSubmit} variant="contained">
+            <Button type="submit" variant="contained">
               Save
             </Button>
           </div>
         </form>
-      ) : (
-        <p>Loading....</p>
-      )}
-    </div>
-  );
 }
